788ef13454d035aaa1ffa09d25c2f2da91e5dbf8
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2023-07-05 22:07:46 +0200

    modal window

--- js/modal.js
+++ js/modal.js
@@ -12,10 +12,3 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
-
-
-
-
-border-radius: 4px;
-background: var(--dairy, #FCFCFC);
-box-shadow: 0px 2px 1px 0px rgba(0, 0, 0, 0.20), 0px 1px 3px 0px rgba(0, 0, 0, 0.12), 0px 1px 1px 0px rgba(0, 0, 0, 0.14);
