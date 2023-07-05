2d863a0e7e1f59dfd1c26b00535d711365349d18
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2023-07-05 16:08:48 +0200

    shadow

---
+++ modal.js
@@ -0,0 +1,21 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+
+
+
+border-radius: 4px;
+background: var(--dairy, #FCFCFC);
+box-shadow: 0px 2px 1px 0px rgba(0, 0, 0, 0.20), 0px 1px 3px 0px rgba(0, 0, 0, 0.12), 0px 1px 1px 0px rgba(0, 0, 0, 0.14);
